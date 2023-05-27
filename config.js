c52fd28db1bb517c919b5fa1cd6913b9ac03246f
CristobalTorres <cristobal.torres6771@gmail.com>
2023-05-27 16:07:25 -0500

    archivos

---
+++ config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Pega tu configuración de firebase aquí
+var firebaseConfig = {
+    apiKey: "AIzaSyCaHDKCqDVjG8uPs_asuNqOMPUiB4OMXPk",
+    authDomain: "proyecto-79.firebaseapp.com",
+    projectId: "proyecto-79",
+    storageBucket: "proyecto-79.appspot.com",
+    messagingSenderId: "942060965941",
+    appId: "1:942060965941:web:c1cf8bd4b7525e9d68217c"
+  };
+
+
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
